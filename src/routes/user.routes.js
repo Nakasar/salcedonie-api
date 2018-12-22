0e0c04403315421c262e23d23f9026f1dc8e97d7
THIZY Kevin <kevin.thizy@outlook.fr>
2018-12-22 11:36:31 +0100

    doc(api definition): Document request body and success responses

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,193 @@ const { requireAuthentication } = require('../middlewares/authentication.middlew
 
 const router = express.Router();
 
+/**
+ * USER SECURITY SCHEME
+ * @swagger
+ *
+ * components:
+ *  securitySchemes:
+ *    user:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
+ */
+
+/**
+ * EVENT SCHEMAS
+ * @swagger
+ *
+ * components:
+ *  schemas:
+ *    EventSummary:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *        title:
+ *          type: string
+ *        description:
+ *          type: string
+ *        active:
+ *          type: boolean
+ *          description: Prevent user from posting actions if false.
+ *        owner:
+ *          type: string
+ *          description: User that own the event.
+ *      example:
+ *        id: '5c1c06cd8d93224570fcc65b'
+ *        title: Prologue
+ *        description: Le commencement de notre aventure !
+ *        active: true
+ *        owner: '5c1c06cd8d93224570fcc65b'
+ *    EventDetails:
+ *      allOf:
+ *        - $ref: '#/components/schemas/EventSummary'
+ *        - type: object
+ *          properties:
+ *            text:
+ *              type: string
+ *          example:
+ *            text: Voici un texte de description complet de l'évènement, avec éventuellement des intrusctions, etc.
+ *    EventCreate:
+ *      type: object
+ *      properties:
+ *        title:
+ *          type: string
+ *        description:
+ *          type: string
+ *        text:
+ *           type: string
+ *        active:
+ *          type: boolean
+ *          description: Prevent user from posting actions if false.
+ *        owner:
+ *          type: string
+ *          description: User that own sthe event. Default to bearer of the token.
+ *      required:
+ *        - title
+ *        - description
+ *      example:
+ *        title: Prologue
+ *        description: Le commencement de notre aventure !
+ *        text: Voici un texte de description complet de l'évènement, avec éventuellement des intrusctions, etc.
+ *        active: true
+ *        owner: '5c1c06cd8d93224570fcc65b'
+ */
+
+/**
+ * CHARACTER SCHEMAS
+ * @swagger
+ *
+ * components:
+ *  schemas:
+ *    CharacterSummary:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *        name:
+ *          type: string
+ *        description:
+ *          type: string
+ *        owner:
+ *          type: string
+ *          description: User that owns the character.
+ *      example:
+ *        id: '5c1c06cd8d93224570fcc65b'
+ *        name: Henry Flore
+ *        description: Le grand héros de tous les temps !
+ *    CharacterDetails:
+ *      allOf:
+ *        - $ref: '#/components/schemas/CharacterSummary'
+ *    CharacterCreate:
+ *      type: object
+ *      properties:
+ *        name:
+ *          type: string
+ *        description:
+ *          type: string
+ *        owner:
+ *          type: string
+ *          description: User that owns the character. Default to bearer of the token.
+ *      required:
+ *        - name
+ *        - description
+ *      example:
+ *        name: Henry Flore
+ *        description: Le grand héros de tous les temps !
+ *        owner: '5c1c06cd8d93224570fcc65b'
+ */
+
+/**
+ * ACTION SCHEMAS
+ * @swagger
+ *
+ * components:
+ *  schemas:
+ *    ActionSummary:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *        title:
+ *          type: string
+ *        event:
+ *          type: string
+ *        date:
+ *          type: string
+ *        time:
+ *          type: string
+ *        location:
+ *          type: string
+ *        description:
+ *          type: string
+ *        character:
+ *          type: string
+ *      example:
+ *        id: '5c1c06cd8d93224570fcc65b'
+ *        title: Un verre au bar
+ *        event: '5c1c06cd8d93224570fcc65b'
+ *        date: jour 12
+ *        time: matin
+ *        location: Au bar de Frid
+ *        description: Henry s'empare d'un verre et le descend d'une traite.
+ *        character: '5c1c06cd8d93224570fcc65b'
+ *    ActionDetails:
+ *      allOf:
+ *        - $ref: '#/components/schemas/CharacterSummary'
+ *    ActionCreate:
+ *      type: object
+ *      properties:
+ *        title:
+ *          type: string
+ *        date:
+ *          type: string
+ *        time:
+ *          type: string
+ *        location:
+ *          type: string
+ *        description:
+ *          type: string
+ *        character:
+ *          type: string
+ *          description: Character that execute the action, default to user default character if set.
+ *      required:
+ *        - title
+ *        - date
+ *        - time
+ *        - location
+ *        - description
+ *      example:
+ *        title: Un verre au bar
+ *        event: '5c1c06cd8d93224570fcc65b'
+ *        date: jour 12
+ *        time: matin
+ *        location: Au bar de Frid
+ *        description: Henry s'empare d'un verre et le descend d'une traite.
+ *        character: '5c1c06cd8d93224570fcc65b'
+ */
+
 router.use(requireAuthentication);
 
 /**
@@ -17,6 +204,15 @@ router.use(requireAuthentication);
  *    operationId: getUserDefaultCharacter
  *    tags:
  *      - users
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: Default character of user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CharacterDetails'
  */
 router.get('/users/:userId/default-character', (req, res) => {
 
@@ -30,6 +226,22 @@ router.get('/users/:userId/default-character', (req, res) => {
  *    operationId: updateUserDefaultCharacter
  *    tags:
  *      - users
+ *    security:
+ *      - user: []
+ *    requestBody:
+ *      description: Default character id to set as default
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: string
+ *            example: '5c1c06cd8d93224570fcc65b'
+ *    responses:
+ *      200:
+ *        description: Default character of user set.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CharacterDetails'
  */
 router.post('/users/:userId/default-character', (req, res) => {
 
@@ -43,6 +255,17 @@ router.post('/users/:userId/default-character', (req, res) => {
  *    operationId: getEvents
  *    tags:
  *      - events
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: List of events the user may access.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/EventSummary'
  */
 router.get('/events', (req, res) => {
 
@@ -56,6 +279,21 @@ router.get('/events', (req, res) => {
  *    operationId: createEvent
  *    tags:
  *      - events
+ *    security:
+ *      - user: []
+ *    requestBody:
+ *      description: Event to create
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/EventCreate'
+ *    responses:
+ *      201:
+ *        description: Event created.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/EventDetails'
  */
 router.post('/events', (req, res) => {
 
@@ -69,6 +307,15 @@ router.post('/events', (req, res) => {
  *    operationId: Get event details and last infos.
  *    tags:
  *      - events
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: Event requested.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/EventDetails'
  */
 router.get('/events/:eventId', (req, res) => {
 
@@ -82,6 +329,22 @@ router.get('/events/:eventId', (req, res) => {
  *    operationId: updateEvent
  *    tags:
  *      - events
+ *    security:
+ *      - user: []
+ *    requestBody:
+ *      description: Event to update
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *    responses:
+ *      200:
+ *        description: Updated Event
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/EventDetails'
  */
 router.patch('/events/:eventId', (req, res) => {
 
@@ -95,6 +358,11 @@ router.patch('/events/:eventId', (req, res) => {
  *    operationId: archiveEvent
  *    tags:
  *      - events
+ *    security:
+ *      - user: []
+ *    responses:
+ *      204:
+ *        description: Event archived
  */
 router.delete('/events/:eventId', (req, res) => {
 
@@ -108,6 +376,17 @@ router.delete('/events/:eventId', (req, res) => {
  *    operationId: Get last actions in event
  *    tags:
  *      - actions
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: List of actions of event.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/ActionSummary'
  */
 router.get('/events/:eventId/actions', (req, res) => {
 
@@ -121,6 +400,22 @@ router.get('/events/:eventId/actions', (req, res) => {
  *    operationId: postAction
  *    tags:
  *      - actions
+ *    security:
+ *      - user: []
+ *    requestBody:
+ *      description: Action to create
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/ActionCreate'
+ *    responses:
+ *      201:
+ *        description: Created Action
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ActionDetails'
  */
 router.post('/events/:eventId/actions', (req, res) => {
 
@@ -134,6 +429,15 @@ router.post('/events/:eventId/actions', (req, res) => {
  *    operationId: getAction
  *    tags:
  *      - actions
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: Requested Action
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ActionDetails'
  */
 router.get('/events/:eventId/actions/:actionId', (req, res) => {
 
@@ -147,6 +451,22 @@ router.get('/events/:eventId/actions/:actionId', (req, res) => {
  *    operationId: updateAction
  *    tags:
  *      - actions
+ *    security:
+ *      - user: []
+ *    requestBody:
+ *      description: Action to update
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *    responses:
+ *      200:
+ *        description: Updated Action
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ActionDetails'
  */
 router.patch('/events/:eventId/actions/:actionId', (req, res) => {
 
@@ -160,6 +480,11 @@ router.patch('/events/:eventId/actions/:actionId', (req, res) => {
  *    operationId: deleteAction
  *    tags:
  *      - actions
+ *    security:
+ *      - user: []
+ *    responses:
+ *      204:
+ *        description: Action deleted
  */
 router.delete('/events/:eventId/actions/:actionId', (req, res) => {
 
@@ -173,6 +498,17 @@ router.delete('/events/:eventId/actions/:actionId', (req, res) => {
  *    operationId: Get characters
  *    tags:
  *      - characters
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: List of characters
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/CharacterSummary'
  */
 router.get('/characters', (req, res) => {
 
@@ -186,6 +522,22 @@ router.get('/characters', (req, res) => {
  *    operationId: createCharacter
  *    tags:
  *      - characters
+ *    security:
+ *      - user: []
+ *    requestBody:
+ *      description: Character to create
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/CharacterCreate'
+ *    responses:
+ *      201:
+ *        description: Created character
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CharacterDetails'
  */
 router.post('/characters', (req, res) => {
 
@@ -199,6 +551,17 @@ router.post('/characters', (req, res) => {
  *    operationId: Get character details
  *    tags:
  *      - characters
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: Requested Character
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/CharacterDetails'
  */
 router.get('/characters/:characterId', (req, res) => {
 
@@ -212,6 +575,24 @@ router.get('/characters/:characterId', (req, res) => {
  *    operationId: updateCharacter
  *    tags:
  *      - characters
+ *    security:
+ *      - user: []
+ *    requestBody:
+ *      description: Character to update
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *    responses:
+ *      200:
+ *        description: Updated Character
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/CharacterDetails'
  */
 router.patch('/characters/:characterId', (req, res) => {
 
@@ -225,6 +606,17 @@ router.patch('/characters/:characterId', (req, res) => {
  *    operationId: getCharacterActions
  *    tags:
  *      - actions
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: List of actions of character.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/ActionSummary'
  */
 router.get('/characters/:characterId/actions', (req, res) => {
 
@@ -238,6 +630,17 @@ router.get('/characters/:characterId/actions', (req, res) => {
  *    operationId: getActions
  *    tags:
  *      - actions
+ *    security:
+ *      - user: []
+ *    responses:
+ *      200:
+ *        description: List of actions
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/ActionSummary'
  */
 router.get('/actions', (req, res) => {
 
